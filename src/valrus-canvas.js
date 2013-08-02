0b6283cd78fd52ff632bc5ebb108478e9c61033d
Björn Skoglund <bjorn.skoglund@valtech.se>
2013-08-02 17:16:28 +0200

    Added fading transition.

--- src/valrus-canvas.js
+++ src/valrus-canvas.js
@@ -33,9 +33,7 @@
 			for (var i = 0; i < images.length; ++i) {
 				imageDiv = images[i].getElementsByTagName('img')[0];
 				// TODO: This might not be necessary.
-				image = new Image();
-				image.src = imageDiv.src;
-				ret.push(image);
+				ret.push(imageDiv);
 			}
 			return ret;
 		}
@@ -55,17 +53,32 @@
 		this.frameSwitchId = -1;
 	}
 
-	function Canvas(carousel, settings) {
-		this.settings = settings;
-		var canvas = document.createElement('canvas');
-		this.id = "canvas_" + carousel.id;
-		canvas.id = this.id;
+	function createCanvas(settings) {
+		canvas = document.createElement('canvas');
 		canvas.width = settings.width;
 		canvas.height = settings.height;
+		return canvas;
+	}
+
+	function createDoubleBuffer(settings) {
+		canvas = createCanvas(settings);
+		canvas.style.display = 'none';
+		return canvas;
+	}
+
+	function ScreenBuffer(carousel, settings) {
+		this.settings = settings;
+		var canvas = createCanvas(settings);
+		this.id = "canvas_" + carousel.id;
+		canvas.id = this.id;
 		carousel.appendChild(canvas);
 		this.context = canvas.getContext("2d");
 
-		this.image = function(url) {
+		var doubleBuffer = createDoubleBuffer(settings);
+		carousel.appendChild(doubleBuffer);
+		this.scratch = doubleBuffer.getContext('2d');
+
+		this.putImage = function(url) {
 			var self = this;
 			var imageObj = new Image();
 		    imageObj.onload = function() {
@@ -75,15 +88,29 @@
 		}
 	}
 
-	function animate(canvas, state, settings) {
-		var blend = scroll;
+	function animate(screenBuffer, state, settings) {
+		var blend = fade;
+
+		function fade(image0, image1, progress) {
+		    var i, p, q, compl;
+		    
+		    p = state.source.data;
+		    q = state.target.data;
+		    
+		    
+		    r = state.result.data;
+		    translucency = progress/100.0;
+		    opacity = 1.0 - translucency
+		    for (i = 0; i < p.length; i += 1) {
+		        r[i] = p[i] * opacity + q[i] * translucency;
+		    }
+		    screenBuffer.context.putImageData(state.result, 0, 0);
+		}
 
 		function scroll(image0, image1, progress) {
-			
 			x = progress*settings.width/100.0;
-
-			canvas.context.drawImage(image0, -x, 0, settings.width, settings.height);
-			canvas.context.drawImage(image1, settings.width - x, 0, settings.width, settings.height);
+			screenBuffer.context.drawImage(image0, -x, 0, settings.width, settings.height);
+			screenBuffer.context.drawImage(image1, settings.width - x, 0, settings.width, settings.height);
 		}
 
 		function nextFrame(state, settings) {
@@ -92,13 +119,11 @@
 
 		function updateProgress(state, settings) {
 			g = settings.speed/300;
-
 			M = 0.0012;
 			drag = Math.max(M*(-85+state.progress), 0);
 			acc = g - drag;
 			speed = Math.min(settings.speed, acc*state.i);
 			return Math.min(state.progress + speed, 100.0);
-
 		}
 
 		function switchFrame() {
@@ -113,9 +138,21 @@
 			++state.i;
 		}
 
+		function readData(image) {
+	        screenBuffer.scratch.drawImage(image, 0, 0, settings.width, settings.height);
+    	    return screenBuffer.scratch.getImageData(0, 0, settings.width, settings.height);
+		}
+
 		function startSwitchFrame() {
+			console.log("Switching frames between " + state.currentFrame + " and " + nextFrame(state, settings));
+
 			interval = 1000 / 60;
 			state.i = 0;
+
+    	    state.source = readData(settings.images[state.currentFrame])
+    	    state.target = readData(settings.images[nextFrame(state, settings)]);
+
+			state.result = screenBuffer.context.createImageData(settings.width, settings.height);
 			state.frameSwitchId = window.setInterval(switchFrame, interval);
 		}
 
@@ -123,18 +160,18 @@
 		console.log("Started animation for " + carousel.id);
 	}
 
-	function bootCarousel(carousel, settings) {
+	function initCarousel(carousel, settings) {
 		carousel.style.width = "" + settings.frameWidth + "px";
 		carousel.style.height = "" + settings.frameHeight + "px";
 		carousel.getElementsByClassName("images")[0].style.display = "none";
 
 
-		var canvas = new Canvas(carousel, settings);
-		canvas.image(settings.images[0].src);
+		var screenBuffer = new ScreenBuffer(carousel, settings);
+		screenBuffer.putImage(settings.images[0].src);
 
 		var state = new State(carousel);
 
-		animate(canvas, state, settings)
+		animate(screenBuffer, state, settings)
 	}
 
 	function main() {
@@ -144,7 +181,7 @@
 			carousel.id = "carousel" + i;
 			console.log("Found carousel [" + i + "/" + carousels.length + "]: " + carousel.id);
 			settings = new Settings(carousel);
-			bootCarousel(carousel, settings);
+			initCarousel(carousel, settings);
 		}
 	}
 
