71ef346d39a7cc74d646a6cdcd34cad28a62cfbc
Björn Skoglund <bjorn.skoglund@valtech.se>
2013-08-06 16:50:22 +0200

    Added jasmine tests for Settings class.

---
+++ test/test_settings.js
@@ -0,0 +1,45 @@
+describe("Settings default values", function() {
+	carousel = document.createElement('canvas');
+	settings = new valrus.Settings(carousel);
+
+	it("Speed", function() {
+		expect(settings.speed).toEqual(8.0);
+	});
+    
+	it("Width", function() {
+		expect(settings.width).toEqual(512);
+	});
+
+	it("Height", function() {
+		expect(settings.height).toEqual(255);
+	});
+
+});
+
+describe("Settings DOM values", function() {
+	
+
+	carousel = setFixtures('<div class="carousel"><div class="settings">' + 
+		'<span class="speed">10</span>' + 
+		'<span class="width">1024</span>' + 
+		'<span class="height">510</span>' + 
+		'<span class="switchPause">1234</span>' + 
+		'</div></div>')[0];
+	var settings = new valrus.Settings(carousel);
+	it("Speed", function() {
+		expect(settings.speed).toEqual(10.0);
+	});
+
+	it("Width", function() {
+		expect(settings.width).toEqual(1024);
+	});
+
+	it("Height", function() {
+		expect(settings.height).toEqual(510);
+	});
+    
+	it("SwitchPause", function() {
+		expect(settings.switchPause).toEqual(1234);
+	});
+});
+
